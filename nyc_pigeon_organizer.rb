d3cc9e6f2f5d034ca37d9c8f40ed7002eecdb7f5
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-25 17:47:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,6 @@ def nyc_pigeon_organizer(data)
         end 
         if !final_array[name][key] 
           !final_array[name][key] = [] 
-          binding.pry
         end
         final_array[name][key].push(inner_key.to_s)
       end
